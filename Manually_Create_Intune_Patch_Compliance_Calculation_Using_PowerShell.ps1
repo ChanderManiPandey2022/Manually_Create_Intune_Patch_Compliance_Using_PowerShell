153f9a034633e46be7cdfc0a732139de14c62d44
ChanderManiPandey2022 <116195110+ChanderManiPandey2022@users.noreply.github.com>
2023-10-25 14:48:00 +0530

    Update Manually_Create_Intune_Patch_Compliance_Calculation_Using_PowerShell.ps1
    Attention, please
    
    Those who are using my custom PowerShell script to generate the "Intune Windows update patching compliance", please note that due to a change in the URL or non-working URL, all Windows 10 machines are showing a 'Manually check' status.
    
    Please find this line in the script:
    
    $URI = 'https://aka.ms/WindowsUpdateHistory';
    
    and update it with this line:
    
    $URI =  "https://support.microsoft.com/en-us/help/4043454";
    
    I will update all impacted scripts and update them on GitHub."
    
    Thank you for your understanding and support
    
    Please also confirm if it's still not working

--- Manually_Create_Intune_Patch_Compliance_Calculation_Using_PowerShell.ps1
+++ Manually_Create_Intune_Patch_Compliance_Calculation_Using_PowerShell.ps1
@@ -109,7 +109,8 @@ $BuildDetails = $buildInfoArray
 Write-Host "Downoading Patch List from Microsoft"-ForegroundColor yellow
 $URI = "https://aka.ms/Windows11UpdateHistory";
 $CollectedData += (Invoke-WebRequest -Uri $URI -UseBasicParsing -ErrorAction Continue).Links;
-$URI = "https://aka.ms/WindowsUpdateHistory";
+#$URI = "https://aka.ms/WindowsUpdateHistory";
+$URI = "https://support.microsoft.com/en-us/help/4043454";
 $CollectedData += (Invoke-WebRequest -Uri $URI -UseBasicParsing -ErrorAction Continue).Links;
 
 #Filter Windows Master Patch List
